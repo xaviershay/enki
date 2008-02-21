0f48ff1652fbeffcdf97bd6f9bfda3b303e8313d
Xavier Shay <xavier@rhnh.net>
2008-02-21 19:10:10 +1100

    Schema for sessions table

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 5) do
+ActiveRecord::Schema.define(:version => 6) do
 
   create_table "comments", :force => true do |t|
     t.integer  "post_id",                                 :null => false
@@ -52,6 +52,16 @@ ActiveRecord::Schema.define(:version => 5) do
   end
 
   add_index "posts", ["published_at"], :name => "index_posts_on_published_at"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :default => "", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id"
