3e1e99479f5d855f2730c150bca9acd907a4d2a4
Xavier Shay <xavier@rhnh.net>
2008-05-03 15:53:05 +1000

    Assign Ctrl+Z and Meta+Z to undo

--- public/javascripts/admin/common.js
+++ public/javascripts/admin/common.js
@@ -6,6 +6,7 @@ jQuery.delegate = function(rules) {
   }
 }
 
+
 function restripe() {
   $('table tr:odd').removeClass('alt'); 
   $('table tr:even').addClass('alt'); 
@@ -21,8 +22,10 @@ function asyncDeleteForm(obj, options) {
     dataType: 'json',
     success: function(msg){
       display = msg.undo_message
-      if (msg.undo_path)
+      if (msg.undo_path) {
         display += '<span class="undo-link"> (<a class="undo-link" href="' + msg.undo_path + '">undo</a>)</span>';
+        undo_stack.push(msg.undo_path);
+      }  
       humanMsg.displayMsg(display);
     },
     error: function (XMLHttpRequest, textStatus, errorThrown) {
@@ -31,31 +34,45 @@ function asyncDeleteForm(obj, options) {
   }, options || {}));
 }
 
+function processUndo(path, options) {
+  $.ajax($.extend({
+    type: "POST",
+    url: path,
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader("Accept", "application/json");
+    },
+    dataType: 'json',
+    success: function(msg){
+      humanMsg.displayMsg( msg.message );
+    },
+    error: function (XMLHttpRequest, textStatus, errorThrown) {
+      humanMsg.displayMsg( 'Could not undo' );
+    }
+  }, options || {}));  
+
+  // Assume success and remove undo link
+  $('a.undo-link[href=' + path + ']').parent('span').hide();
+}
+
 function asyncUndoBehaviour(options) {
   $('#humanMsgLog').click($.delegate({
     'a.undo-link': function(e) { 
-      link = jQuery(e.target);
-      $.ajax($.extend({
-        type: "POST",
-        url: link.attr('href'),
-         beforeSend: function(xhr) {
-           xhr.setRequestHeader("Accept", "application/json");
-         },
-         dataType: 'json',
-         success: function(msg){
-           humanMsg.displayMsg( msg.message );
-         },
-         error: function (XMLHttpRequest, textStatus, errorThrown) {
-           humanMsg.displayMsg( 'Could not undo' );
-         }
-       }, options || {}));  
-
-       // Assume success and remove undo link
-       link.parent('span').hide();
-       return false;
+      processUndo(jQuery(e.target).attr('href'), options);
+      return false;
     }
   }));
+  jQuery.each(["Ctrl+Z", "Meta+Z"], function () {
+    shortcut.add(this, function() { 
+      item = undo_stack.pop();
+      if (item)
+        processUndo(item, options) 
+      else
+        humanMsg.displayMsg("Nothing to undo");
+    });
+  });  
 }
+
+var undo_stack = new Array();
 
 function destroyAndUndoBehaviour(type) {
   return function (){
