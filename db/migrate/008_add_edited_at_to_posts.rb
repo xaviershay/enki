e6085b093cbb6cd31708df55bd0e5bc7a03e7ae2
Mark Gulbrandsen <gulbrand@mbpro-2.local>
2008-09-13 13:18:51 -0600

    Add default value for edited_at column
    Without this, a clean clone doesn't migrate on VERSION=8.
    I'm not sure this is the proper solution, but it seems ok,
    especially since the next command sets all of these values
    equal to updated_at.
    
    I wonder if there's a better way to do this, but I don't
    understand migrations as well as I should.

--- db/migrate/008_add_edited_at_to_posts.rb
+++ db/migrate/008_add_edited_at_to_posts.rb
@@ -2,7 +2,7 @@ class AddEditedAtToPosts < ActiveRecord::Migration
   class Post < ActiveRecord::Base; end;
 
   def self.up
-    add_column :posts, :edited_at, :datetime, :null => false
+    add_column :posts, :edited_at, :datetime, :null => false, :default => Time.now
     Post.update_all('edited_at = updated_at')
   end
 
