6ffdd96d52a39c15cc312669d5307d120596c69b
Xavier Shay <xavier@rhnh.net>
2008-09-18 19:31:10 -0700

    Collapse migrations to 009, added schema that matches sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,5 @@
 # This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of ActiveRecord to incrementally modify your database, and
+# please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your database schema. If you need
@@ -12,37 +12,19 @@
 ActiveRecord::Schema.define(:version => 9) do
 
   create_table "comments", :force => true do |t|
-    t.integer  "post_id",                                    :null => false
-    t.string   "author",                  :default => "",    :null => false
-    t.string   "author_url",              :default => "",    :null => false
-    t.string   "author_email",            :default => "",    :null => false
-    t.string   "author_openid_authority", :default => "",    :null => false
-    t.text     "body",                    :default => "",    :null => false
-    t.text     "body_html",               :default => "",    :null => false
+    t.integer  "post_id",                 :null => false
+    t.string   "author",                  :null => false
+    t.string   "author_url",              :null => false
+    t.string   "author_email",            :null => false
+    t.string   "author_openid_authority", :null => false
+    t.text     "body",                    :null => false
+    t.text     "body_html",               :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "spam",                    :default => false
-    t.float    "spaminess"
-    t.string   "signature"
-    t.string   "author_ip",               :default => "",    :null => false
-    t.string   "author_referer",          :default => "",    :null => false
   end
 
   add_index "comments", ["created_at"], :name => "index_comments_on_created_at"
   add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
-  add_index "comments", ["spaminess"], :name => "index_comments_on_spaminess"
-
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority",     :default => 0
-    t.integer  "attempts",     :default => 0
-    t.text     "handler"
-    t.string   "last_error"
-    t.datetime "run_at"
-    t.datetime "locked_until"
-    t.string   "locked_by"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "open_id_authentication_associations", :force => true do |t|
     t.integer "issued"
@@ -54,16 +36,16 @@ ActiveRecord::Schema.define(:version => 9) do
   end
 
   create_table "open_id_authentication_nonces", :force => true do |t|
-    t.integer "timestamp",                  :null => false
+    t.integer "timestamp",  :null => false
     t.string  "server_url"
-    t.string  "salt",       :default => "", :null => false
+    t.string  "salt",       :null => false
   end
 
   create_table "pages", :force => true do |t|
-    t.string   "title",      :default => "", :null => false
-    t.string   "slug",       :default => "", :null => false
-    t.text     "body",       :default => "", :null => false
-    t.text     "body_html",  :default => "", :null => false
+    t.string   "title",      :null => false
+    t.string   "slug",       :null => false
+    t.text     "body",       :null => false
+    t.text     "body_html",  :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -72,10 +54,10 @@ ActiveRecord::Schema.define(:version => 9) do
   add_index "pages", ["title"], :name => "index_pages_on_title"
 
   create_table "posts", :force => true do |t|
-    t.string   "title",                   :default => "",   :null => false
-    t.string   "slug",                    :default => "",   :null => false
-    t.text     "body",                    :default => "",   :null => false
-    t.text     "body_html",               :default => "",   :null => false
+    t.string   "title",                                     :null => false
+    t.string   "slug",                                      :null => false
+    t.text     "body",                                      :null => false
+    t.text     "body_html",                                 :null => false
     t.boolean  "active",                  :default => true, :null => false
     t.integer  "approved_comments_count", :default => 0,    :null => false
     t.string   "cached_tag_list"
@@ -87,13 +69,8 @@ ActiveRecord::Schema.define(:version => 9) do
 
   add_index "posts", ["published_at"], :name => "index_posts_on_published_at"
 
-  create_table "schema_migrations", :primary_key => "version", :force => true do |t|
-  end
-
-  add_index "schema_migrations", ["version"], :name => "unique_schema_migrations", :unique => true
-
   create_table "sessions", :force => true do |t|
-    t.string   "session_id", :default => "", :null => false
+    t.string   "session_id", :null => false
     t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -119,8 +96,8 @@ ActiveRecord::Schema.define(:version => 9) do
   add_index "tags", ["name"], :name => "index_tags_on_name"
 
   create_table "undo_items", :force => true do |t|
-    t.string   "type",       :default => "", :null => false
-    t.datetime "created_at",                 :null => false
+    t.string   "type",       :null => false
+    t.datetime "created_at", :null => false
     t.text     "data"
   end
 
