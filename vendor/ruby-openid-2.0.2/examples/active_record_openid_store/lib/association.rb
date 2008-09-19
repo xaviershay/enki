248ae90f4b5a3be7a2179096fc68849d3a3315f1
Xavier Shay <xavier@rhnh.net>
2008-09-18 18:45:13 -0700

    Upgrade ruby-openid from 2.0.2 to 2.1.2

--- vendor/ruby-openid-2.0.2/examples/active_record_openid_store/lib/association.rb
+++
@@ -1,9 +0,0 @@
-require 'openid/association'
-
-class Association < ActiveRecord::Base
-  set_table_name 'open_id_associations'
-  def from_record
-    OpenID::Association.new(handle, secret, issued, lifetime, assoc_type)
-  end
-end
-
--- vendor/ruby-openid-2.0.2/lib/openid/protocolerror.rb
+++
@@ -1,6 +0,0 @@
-module OpenID
-
-  # An error in the OpenID protocol
-  class ProtocolError < Exception
-  end
-end
