f90b0d67c7f8f2b9f48412fdd6e8cfcca5d00ce4
Xavier Shay <xavier@rhnh.net>
2008-09-18 18:02:26 -0700

    Add comment about bogus default

--- db/migrate/008_add_edited_at_to_posts.rb
+++ db/migrate/008_add_edited_at_to_posts.rb
@@ -2,7 +2,8 @@ class AddEditedAtToPosts < ActiveRecord::Migration
   class Post < ActiveRecord::Base; end;
 
   def self.up
-    add_column :posts, :edited_at, :datetime, :null => false, :default => Time.now
+    # this is a bogus default to keep SQLite happy, remove it when we collapse migrations
+    add_column :posts, :edited_at, :datetime, :null => false, :default => Time.now 
     Post.update_all('edited_at = updated_at')
   end
 
