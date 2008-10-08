4ac99a608d40ef411ffd4b4152669a964125d58e
Jason Cheow <jason@jasoncheow.com>
2008-10-08 23:10:00 +0800

    Modify specs to accomodate default pagination of posts.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -17,10 +17,11 @@ describe Post, ".find_recent" do
   it 'finds the most recent posts that were published before now' do
     now = Time.now
     Time.stub!(:now).and_return(now)
-    Post.should_receive(:find).with(:all, {
+    Post.should_receive(:paginate).with({
+      :page       => nil,
       :order      => 'posts.published_at DESC',
       :conditions => ['published_at < ?', now],
-      :limit      => Post::DEFAULT_LIMIT
+      :per_page   => Post::DEFAULT_LIMIT
     })
     Post.find_recent
   end
@@ -28,10 +29,11 @@ describe Post, ".find_recent" do
   it 'finds the most recent posts that were published before now with a tag' do
     now = Time.now
     Time.stub!(:now).and_return(now)
-    Post.should_receive(:find_tagged_with).with('code', {
+    Post.should_receive(:paginate_tagged_with).with('code', {
+      :page       => nil,
       :order      => 'posts.published_at DESC',
       :conditions => ['published_at < ?', now],
-      :limit      => Post::DEFAULT_LIMIT
+      :per_page   => Post::DEFAULT_LIMIT
     })
     Post.find_recent(:tag => 'code')
   end
