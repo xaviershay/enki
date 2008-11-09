baf6a1421eca476a1f629fba9254830b15cbc41b
Jason Cheow <jason@jasoncheow.com>
2008-11-09 11:49:37 +0800

    Fix bug that occurs when tag is destroyed but associated taggables' cached_tag_list isn't updated.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,6 +4,18 @@ class Tag < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
   
+  # TODO: Contribute this back to acts_as_taggable_on_steroids plugin
+  # Update taggables' cached_tag_list
+  after_destroy do |tag|
+    tag.taggings.each do |tagging|
+      taggable = tagging.taggable
+      if taggable.class.caching_tag_list?
+        taggable.tag_list = TagList.new(*taggable.tags.map(&:name))
+        taggable.save
+      end
+    end
+  end
+  
   cattr_accessor :destroy_unused
   self.destroy_unused = false
   
