12d03ca62f6bb7277ba2f463046d762838d13b12
Jason Cheow <jason@jasoncheow.com>
2008-11-16 07:08:50 +0800

    Add feature to pause/start live edit preview.

--- public/javascripts/admin/live-edit-preview.js
+++ public/javascripts/admin/live-edit-preview.js
@@ -5,8 +5,12 @@ $(document).ready(function() {
     var dest = window.location.href;
 
     if (!dest.endsWith('new')) {
-      if (dest.match(/\/\d$/)) { dest = dest.replace(/\/\d$/, '/new') }
-      else { dest = dest + '/new' }
+      if (dest.match(/\/\d$/)) { 
+        dest = dest.replace(/\/\d$/, '/new')
+      }
+      else { 
+        dest = dest + '/new'
+      }
     }
 
     jQuery.ajax({
@@ -18,15 +22,30 @@ $(document).ready(function() {
       },
       success: function(r) { 
         if ($('#preview').length == 0) {
-          form.after('<div id="preview"><h3>Your entry will be formatted like this:</h3><div class="content"></div></div>')
+          form.after('<div id="preview"><h3>Your entry will be formatted like this: <span>(pause live preview)</span></h3><div class="content"></div></div>');
         }
-        $('#preview .content').html(r)
+        $('#preview .content').html(r);
       }
     });
   };
 
-  input_elements.keyup(function () {
-    fetch_preview.only_every(1000);
+  $('#preview h3 span').livequery('click', function() {
+    var text = $(this).html();
+    if (text.match(/pause/)) {
+      $(this).html(text.replace(/pause/, 'start'));
+      $('#preview .content').fadeTo(100, 0.3);
+    }
+    else {
+      $(this).html(text.replace(/start/, 'pause'));
+      $('#preview .content').fadeTo(100, 1);
+      fetch_preview();
+    }
+  });
+
+  input_elements.keyup(function() {
+    if ($('#preview h3 span').html().match(/pause/)) {
+      fetch_preview.only_every(1000);
+    }
   });
 
   fetch_preview();
