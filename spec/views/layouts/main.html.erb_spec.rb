2f0db1a592645fa386cc32182e6399bef065ac3b
Xavier Shay <xavier@rhnh.net>
2008-12-01 20:20:26 +1100

    h() some stuff in the main layout - this test doesn't actually fail, there seems to be an issue with safe_erb not raising in tests (fairly certain it's the rails 2.2 upgrade that broke it)

--- spec/views/layouts/main.html.erb_spec.rb
+++ spec/views/layouts/main.html.erb_spec.rb
@@ -3,10 +3,16 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 describe "/layouts/main.html.erb" do
   before(:each) do
     mock_tag = mock_model(Tag,
-      :name     => 'code',
+      :name     => 'code'.taint,
       :taggings => [mock_model(Tagging)]
     )
     Tag.stub!(:find).and_return([mock_tag])
+
+    mock_page = mock_model(Page,
+      :title     => 'about'.taint,
+      :slug     => 'about'.taint
+    )
+    Page.stub!(:find).and_return([mock_page])
   end
 
   it 'renders' do
