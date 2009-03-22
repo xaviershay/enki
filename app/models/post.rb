672b986e4531a30af4adeab5c9e870b1b8a1f22b
Xavier Shay <xavier@rhnh.net>
2009-03-22 16:27:11 +1100

    Fix post detection - probably something more robust with timezones is needed here

--- app/models/post.rb
+++ app/models/post.rb
@@ -66,7 +66,9 @@ class Post < ActiveRecord::Base
       begin
         day = Time.parse([year, month, day].collect(&:to_i).join("-")).midnight
         post = find_all_by_slug(slug, options).detect do |post|
-          post.published_at.midnight == day
+          [:year, :month, :day].all? {|time|
+            post.published_at.send(time) == day.send(time)
+          }
         end 
       rescue ArgumentError # Invalid time
         post = nil
