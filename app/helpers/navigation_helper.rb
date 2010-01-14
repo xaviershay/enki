fe54d016e5dde3b83e7cb6c906bf3418d263c146
Paul Dowman <paul@pauldowman.com>
2010-01-14 17:30:02 -0500

    The "Home" link in the sidebar shouldn't assume the posts controller is mapped to the root.

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,7 +1,7 @@
 module NavigationHelper
   def page_links_for_navigation
     link = Struct.new(:name, :url)
-    [link.new("Home", posts_path),
+    [link.new("Home", root_path),
      link.new("Archives", archives_path)] +
       Page.find(:all, :order => 'title').collect {|page| link.new(page.title, page_path(page))}
   end
