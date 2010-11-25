98cfaff0fa521f213ef818c2a5b3cc38a1641b48
Xavier Shay <xavier@rhnh.net>
2010-11-25 18:58:56 +1100

    String#slugorize now allows apostrophes, and correctly strips multiple dashes at the start and end of the string.

--- lib/core_extensions/string.rb
+++ lib/core_extensions/string.rb
@@ -1,19 +1,21 @@
-module CoreExtensions::String
+module CoreExtensions
+  module String
   
-  def slugorize
-    result = self.downcase
-    result.gsub!(/&([0-9a-z#])+;/, '')  # Ditch Entities
-    result.gsub!('&', 'and')     # Replace & with 'and'
-    result.gsub!(/[^a-z0-9\-]/, ' ') # Get rid of anything we don't like
-    result.gsub!(/\ +/, '-')    # replace all white space sections with a dash
-    result.gsub!(/(-)$/, '')    # trim dashes
-    result.gsub!(/^(-)/, '')    # trim dashes
-    result.gsub!(/(-)+/, '-')    # collapse dashes
-    result
-  end
+    def slugorize
+      result = self.downcase
+      result.gsub!(/&([0-9a-z#])+;/, '')  # Ditch Entities
+      result.gsub!('&', 'and')            # Replace & with 'and'
+      result.gsub!(/[^a-z0-9\-']/, '-')   # Get rid of anything we don't like
+      result.gsub!(/-+/, '-')             # collapse dashes
+      result.gsub!(/-$/, '')              # trim dashes
+      result.gsub!(/^-/, '')              # trim dashes
+      result
+    end
 
-  def slugorize!
-    self.replace(self.slugorize)
+    def slugorize!
+      self.replace(self.slugorize)
+    end
+
   end
 end
 
