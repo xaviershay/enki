3ba5fca9e073cddf4384db754b5839c5e64e9153
Xavier Shay <xavier@rhnh.net>
2010-12-03 14:35:54 +1100

    More specific message for the one remaining pending spec - still fails on 1.9.2

--- spec/controllers/pages_routing_spec.rb
+++ spec/controllers/pages_routing_spec.rb
@@ -3,7 +3,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe PagesController do
   describe "route" do
     it "should not route /pages to show" do
-      pending("This assertion does not seem to be doing what you would expect it to do")
+      pending("This assertion fails under 1.9.2, but not under 1.8.7. No idea why.")
       {:get => "/pages"}.should_not route_to(:controller => 'pages', :action => 'show')
     end
 
