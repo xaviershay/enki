f85b82618b78fe9592e89a184de36ea56300ae3e
Xavier Shay <xavier@rhnh.net>
2011-02-10 07:05:32 +1100

    Fix for the bundler/psych cock up on 1.9.2p136

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+YAML::ENGINE.yamler = "syck" if YAML.const_defined?("ENGINE")
+
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
