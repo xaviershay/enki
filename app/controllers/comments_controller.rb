4ba419bb1cc4a06ab2e5fc3337de26bd646341e4
Xavier Shay <xavier@rhnh.net>
2011-07-23 12:11:27 +1000

    Reformat CommentsController to be easier to read.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,20 +26,30 @@ class CommentsController < ApplicationController
 
   # TODO: Spec OpenID with cucumber and rack-my-id
   def create
-    @comment = Comment.new((session[:pending_comment] || params[:comment] || {}).reject {|key, value| !Comment.protected_attribute?(key) })
+    @comment = Comment.new (session[:pending_comment] || params[:comment] || {}).
+      reject {|key, value| !Comment.protected_attribute?(key) }
     @comment.post = @post
 
     session[:pending_comment] = nil
 
     if @comment.requires_openid_authentication?
       session[:pending_comment] = params[:comment]
-      authenticate_with_open_id(@comment.author, :optional => [:nickname, :fullname, :email]) do |result, identity_url, registration|
+      authenticate_with_open_id(@comment.author,
+        :optional => [:nickname, :fullname, :email]
+      ) do |result, identity_url, registration|
         if result.status == :successful
           @comment.post = @post
 
-          @comment.author_url   = @comment.author
-          @comment.author       = (registration["fullname"] || registration["nickname"] || @comment.author_url).to_s
-          @comment.author_email = (registration["email"] || @comment.author_url).to_s
+          @comment.author_url = @comment.author
+          @comment.author = (
+            registration["fullname"] ||
+            registration["nickname"] ||
+            @comment.author_url
+          ).to_s
+          @comment.author_email = (
+            registration["email"] ||
+            @comment.author_url
+          ).to_s
 
           @comment.openid_error = ""
           session[:pending_comment] = nil
@@ -51,7 +61,8 @@ class CommentsController < ApplicationController
       @comment.blank_openid_fields
     end
 
-    unless response.headers[Rack::OpenID::AUTHENTICATE_HEADER] # OpenID gem already provided a response
+    # #authenticate_with_open_id may have already provided a response
+    unless response.headers[Rack::OpenID::AUTHENTICATE_HEADER]
       if @comment.save
         redirect_to post_path(@post)
       else
@@ -63,7 +74,9 @@ class CommentsController < ApplicationController
   protected
 
   def find_post
-    @post = Post.find_by_permalink(*[:year, :month, :day, :slug].collect {|x| params[x] })
+    @post = Post.find_by_permalink(*[:year, :month, :day, :slug].map {|x|
+      params[x]
+    })
   end
 
   def verify_authenticity_token_unless_openid
