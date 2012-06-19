92d6bcc25ec8d78ba30e7eb97cf55f80e1b05684
Joan Roig <roigarderiu@gmail.com>
2012-06-18 21:39:59 -0700

    i18n for Spain added

--- config/application.rb
+++ config/application.rb
@@ -41,8 +41,8 @@ module Enki
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :es
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
