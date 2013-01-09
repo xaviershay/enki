923f751db5f424c3f397e862cdb1b260568c4b57
Xavier Shay <xavier@rhnh.net>
2013-01-08 18:19:57 -0800

    Fix for CVE-2013-0156

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,11 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::YAML)
+
+ActiveSupport::XmlMini::PARSING.delete("symbol")
+ActiveSupport::XmlMini::PARSING.delete("yaml")
+
 # Initialize the rails application
 Enki::Application.initialize!
