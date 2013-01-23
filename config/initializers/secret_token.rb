2d0d06c0ff295791c860d573de6ae1671710e05a
Xavier Shay <xavier@rhnh.net>
2013-01-22 21:57:24 -0800

    Remove default secret token.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Enki::Application.config.secret_token = '1481948abe0b49729e01200f00033b021cf2e7e1128d37da0cb637c1b661f99cf3bc163c5ba57f164c02254d9255591358c502ddeb1107fe2647af3c5bfa0c37'
+if Rails.env.development? || Rails.env.test?
+  Enki::Application.config.secret_token = SecureRandom.hex(20)
+else
+  # Your secret key for verifying the integrity of signed cookies.
+  # If you change this key, all old signed cookies will become invalid!
+  # Make sure the secret is at least 30 characters and all random,
+  # no regular words or you'll be exposed to dictionary attacks.
+  raise "Generate a secret with `rake secret` and paste it into `config/initializers/secret_token.rb`."
+
+  # If this is an open source project, DO NOT commit your secret to source
+  # control. Load it from ENV or a file that is git ignored (File.read)
+  Enki::Application.config.secret_token = 'paste your token here'
+end
