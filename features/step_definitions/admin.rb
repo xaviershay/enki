47a7564d511fe967f9e1de2c217216e1eee424d7
Gaelian Ditchburn <gaelian.ditchburn@gmail.com>
2015-04-17 19:53:46 +1000

    Fixed cucumber feature definitions.

--- features/step_definitions/admin.rb
+++ features/step_definitions/admin.rb
@@ -1,8 +1,7 @@
 Given /I am logged in/ do
   # post '/admin/session', :bypass_login => '1'
   visit '/admin/session'
-  check "Bypass credentials check"
-  click_on "Login with OpenID"
+  click_on "Bypass credentials check"
 end
 
 Then /a RuntimeError is thrown when I press "(.*)"/ do |button|
